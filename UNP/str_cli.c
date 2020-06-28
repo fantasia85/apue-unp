932d8d3377c534dacfeacb36395306e81cf12e04
hiya <541799565@qq.com>
2020-06-28 13:19:03 +0800

    tcpcli1

--- UNP/str_cli.c
+++ UNP/str_cli.c
@@ -7,7 +7,7 @@ void str_cli(FILE *fp, int sockfd)
 
     while (fgets(sendline, MAXLINE, fp) != NULL) {
         writen(sockfd, sendline, strlen(sendline));
-        if (n = read(sockfd, recvline, MAXLINE) < 0)
+        if ((n = read(sockfd, recvline, MAXLINE)) < 0)
             err_quit("str_cli: server terminated prematurely");
         recvline[n] = '\0';
         fputs(recvline, stdout);
